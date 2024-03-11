2001fa1f888e85a8c039d6ecf9f46040fb2ea6f6
Kumar9525 <92426822+Kumar9525@users.noreply.github.com>
2024-03-11 10:34:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
+function mincost(arr) {
+    let minCost = 0;
   
+    // Sort the array of rope lengths in non-decreasing order
+    arr.sort((a, b) => a - b);
+  
+    // Loop until there is only one rope left
+    while (arr.length > 1) {
+        // Connect the two smallest ropes
+        let sum = arr[0] + arr[1];
+        minCost += sum;
+        
+        // Remove the two smallest ropes and insert the sum
+        arr.splice(0, 2, sum);
+  
+        // Re-sort the array after the merge
+        arr.sort((a, b) => a - b);
+    }
+  
+    return minCost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
+
